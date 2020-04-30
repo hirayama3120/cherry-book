d8110c902f66a15e1483cf666263c8c99fc0e9cd
Your Name <you@example.com>
2020-04-30 18:59:49 +0900

    5.5.3

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,14 @@
-def convert_length(length, unit_form, unit_to)
-  units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
-  (length / units[unit_form] * units[unit_to]).round(2)
+#def convert_length(length, unit_form, unit_to)
+#  units = { m: 1.0, ft: 3.28, in: 39.37 }
+#  (length / units[unit_form] * units[unit_to]).round(2)
+#end
+
+#def convert_length(length, form: :m, to: :m)
+#  units = { m: 1.0, ft: 3.28, in: 39.37 }
+#  (length / units[form] * units[to]).round(2)
+#end
+
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }
+def convert_length(length, form: :m, to: :m)
+  (length / UNITS[form] * UNITS[to]).round(2)
 end
